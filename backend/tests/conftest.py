4326c4c369ecd19efcd22397c9a1bca24c9ce406
droslik <d.roslik@gmail.com>
2024-02-11 11:56:35 +0300

    fixed on api tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,23 @@
-from pytest_factoryboy import register
-
+import pytest
+from unittest.mock import MagicMock
 from .factories import UserFactory, BookFactory
 
-register(UserFactory)
-register(BookFactory)
+
+@pytest.fixture()
+def user_factory():
+    def create_user():
+        return UserFactory
+    yield create_user()
+
+
+@pytest.fixture()
+def book_factory(user_factory):
+    def create_book():
+        return BookFactory
+    yield create_book()
+
+
+@pytest.fixture()
+def mock_requests():
+    requests = MagicMock()
+    return requests.get()
